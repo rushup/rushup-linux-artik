2714709248562664d12ba2e460cc7fd6dce213ed
Matteo Fumagalli <matteofumagalli1275@gmail.com>
2018-03-29 10:33:45 +0200

    dwc2: fix otg issue with hubs

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -62,7 +62,10 @@ static const struct dwc2_core_params params_bcm2835 = {
 	.otg_cap			= 0,	/* HNP/SRP capable */
 	.otg_ver			= 0,	/* 1.3 */
 	.dma_enable			= 1,
+#if defined(CONFIG_KITRAGTI)
 	.dma_desc_enable		= 0,
+#else
+	.dma_desc_enable		= 1,
 	.speed				= 0,	/* High Speed */
 	.enable_dynamic_fifo		= 1,
 	.en_multiple_tx_fifo		= 1,
@@ -123,7 +126,7 @@ static const struct dwc2_core_params params_nexell = {
 	.otg_cap			= 0,	/* HNP/SRP capable */
 	.otg_ver			= 0,	/* 1.3 */
 	.dma_enable			= 1,
-	.dma_desc_enable		= 1,
+	.dma_desc_enable		= 0,
 	.speed				= 0,	/* High Speed */
 	.enable_dynamic_fifo		= 1,
 	.en_multiple_tx_fifo		= 1,
